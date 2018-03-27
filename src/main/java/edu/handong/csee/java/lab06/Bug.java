730f7ca1618bdd5cc3a844a42c5afa850659adf8
JeongSeop <wjdtjq1121@gmail.com>
2018-03-27 21:58:46 +0900

    declare members

--- src/main/java/edu/handong/csee/java/lab06/Bug.java
+++ src/main/java/edu/handong/csee/java/lab06/Bug.java
@@ -1,13 +1,27 @@
-package edu.handong.csee.java.lab06;
+package edu.handong.csee.java.lab06; // brings the package
+
+import java.util.Scanner; 
 
 public class Bug {
 
-	public Bug() {
-		// TODO Auto-generated constructor stub
-	}
-
+	final static double GROWTH_RATE = 0.95;
+	final static double ONE_BUG_VOLUME = 0.002;
+	
+	int houseVolume;
+	int startPopulation;
+	
+	int countWeeks;
+	int population;
+	int totalBugVolume;
+	int newBugs;
+	int BugVolume;
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
