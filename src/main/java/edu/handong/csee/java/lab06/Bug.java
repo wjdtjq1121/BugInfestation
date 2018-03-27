f2f868500edebc19a6a5417dbd7954bd234bfa95
JeongSeop <wjdtjq1121@gmail.com>
2018-03-27 22:30:02 +0900

    program runs properly now

--- src/main/java/edu/handong/csee/java/lab06/Bug.java
+++ src/main/java/edu/handong/csee/java/lab06/Bug.java
@@ -7,15 +7,6 @@ public class Bug {
 	final static double GROWTH_RATE = 0.95;
 	final static double ONE_BUG_VOLUME = 0.002;
 	
-	int houseVolume;
-	int startPopulation;
-	
-	int countWeeks;
-	int population;
-	int totalBugVolume;
-	int newBugs;
-	int BugVolume;
-	
 	
 	
 	
@@ -23,6 +14,39 @@ public class Bug {
 	
 	public static void main(String[] args) {
 
+		double houseVolume;
+		double startPopulation;
+		int countWeeks = 0;
+		double population;
+		double totalBugVolume;
+		double newBugs;
+		// double BugVolume;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Enter the house volume");
+		houseVolume = keyboard.nextDouble();
+
+		System.out.print("Enter the initial value of roaches");
+		startPopulation = keyboard.nextInt();
+		
+		population = startPopulation;
+		totalBugVolume = population*ONE_BUG_VOLUME;
+		
+		
+		while(totalBugVolume < houseVolume) {
+			
+			newBugs = population * GROWTH_RATE;
+			population+=newBugs;
+			totalBugVolume = population * ONE_BUG_VOLUME;
+			
+			countWeeks++;
+		}
+		
+		System.out.printf("result value. countweeks: %d, totalBug: %f, population: %f", countWeeks, totalBugVolume, population);
+		
+		
+		
 	}
 
 }
