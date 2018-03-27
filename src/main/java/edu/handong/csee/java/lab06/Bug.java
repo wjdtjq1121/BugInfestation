5d06ac2b07f381247c02a46d7f46577f375a2478
JeongSeop <wjdtjq1121@gmail.com>
2018-03-27 21:23:34 +0900

    inital commit start

---
+++ src/main/java/edu/handong/csee/java/lab06/Bug.java
@@ -0,0 +1,14 @@
+package edu.handong.csee.java.lab06;
+
+public class Bug {
+
+	public Bug() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
